39b79cee54d1caaea4bcd01eee8fd83a47e2afae
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-11-10 19:40:57 +0900

    update devDependencies
    Highlight: ESLint v3 -> v4
    
    https://eslint.org/blog/2017/06/eslint-v4.0.0-released
    https://eslint.org/docs/user-guide/migrating-to-4.0.0

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var inspect = require('util').inspect;
 
 module.exports = function isResolvable(moduleId) {
   if (typeof moduleId !== 'string') {
-    throw new TypeError(
-      inspect(moduleId) +
-      ' is not a string. Expected a valid Node module identifier, for example `eslint`, `./index.js`, `./lib`.'
-    );
+    throw new TypeError(inspect(moduleId) + ' is not a string. Expected a valid Node.js module identifier (<string>), for example \'eslint\', \'./index.js\', \'./lib\'.');
   }
 
   try {
--- test.js
+++ test.js
@@ -4,55 +4,51 @@ const isResolvable = require('.');
 const test = require('tape');
 
 test('isResolvable()', t => {
-  t.plan(12);
+  t.equal(isResolvable('fs'), true, 'should regard a native module ID as resolvable.');
 
-  t.equal(isResolvable.name, 'isResolvable', 'should have a function name.');
-
-  t.strictEqual(isResolvable('fs'), true, 'should regard a native module ID as resolvable.');
-
-  t.strictEqual(
+  t.equal(
     isResolvable('tryit'),
     true,
     'should regard an installed module ID as resolvable.'
   );
 
-  t.strictEqual(
+  t.equal(
     isResolvable('eslint/bin/eslint'),
     true,
     'should regard a nested file path under the installed module ID as resolvable.'
   );
 
-  t.strictEqual(
+  t.equal(
     isResolvable('./test.js'),
     true,
     'should regard a relative file path beginning with `./` as resolvable.'
   );
 
-  t.strictEqual(
+  t.equal(
     isResolvable(__filename),
     true,
     'should regard an absolute file path as resolvable.'
   );
 
-  t.strictEqual(
+  t.equal(
     isResolvable('.'),
     true,
     'should regard a parent directory path of index.js as resolvable.'
   );
 
-  t.strictEqual(
+  t.equal(
     isResolvable('array-duplicated'),
     false,
     'should regard a module ID that isn\'t installed as unresolvable.'
   );
 
-  t.strictEqual(
+  t.equal(
     isResolvable('./index.json'),
     false,
     'should regard a relative path where no file exists as unresolvable.'
   );
 
-  t.strictEqual(
+  t.equal(
     isResolvable('node_modules'),
     false,
     'should regard a directory path that doesn\'t have index.js as unresolvable.'
@@ -66,7 +62,9 @@ test('isResolvable()', t => {
 
   t.throws(
     () => isResolvable(),
-    /^TypeError.* Expected a valid Node module identifier, for example `eslint`, `\.\/index\.js`, `\.\/lib`\./,
+    /^TypeError.* Expected a valid Node\.js module identifier \(<string>\), for example 'eslint', '\.\/index\.js', '\.\/lib'\./,
     'should throw a type error when it takes no arguments.'
   );
+
+  t.end();
 });
